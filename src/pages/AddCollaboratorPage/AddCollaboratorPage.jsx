e7c9023e8a41b29f181ea07ec8ce5fe955e98bb4
AntonioMarban <393tony393@gmail.com>
2024-05-02 11:27:04 -0600

    Navbar added to /add-collaborator

--- src/pages/AddCollaboratorPage/AddCollaboratorPage.jsx
+++ src/pages/AddCollaboratorPage/AddCollaboratorPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AddCollaboratorForm from "../../components/AddCollaboratorForm/AddCollaboratorForm";
+import Navbar from "../../components/Navbar/Navbar";
 import "./AddCollaboratorPage.css"; // Import the CSS file
 import logoAulify from "../../assets/logoAulify1.png"; // Import the image
 
 function AddCollaboratorPage() {
   return (
     <div className="add-collaborator-page-container">
+      <Navbar />
       <img src={logoAulify} alt="Logo" className="add-collaborator-page-logo" />
       <div className="add-collaborator-page-form-container">
         <AddCollaboratorForm />
